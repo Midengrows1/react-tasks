6681fb92cf4e6da0f280dc97f696ec68b6862b71
Midengrows1 <midengrows@outlook.com>
2025-04-22 20:04:49 +0500

    feat: create an instruction slider component for making pelmeny

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import s from './App.module.css';
-type TValue = string | null | undefined;
-interface IList {
-  id: number;
-  value: TValue;
-}
+import Instruction from './components/Instruction/Instruction';
+// type TValue = string | null | undefined;
+// interface IList {
+//   id: number;
+//   value: TValue;
+// }
 function App() {
-  const [value, setValue] = useState<TValue>('');
-  const [list, setList] = useState<IList[] | []>([]);
-  const [error, setError] = useState<string>('');
-  const [isValueVaild, setIsValueValid] = useState<boolean>(false);
-  const onInputButtonClick = () => {
-    const requestValue: string | undefined | null = prompt('Введите значение');
-    if (requestValue!.length < 3) {
-      setError('Введенное значение должно содержать минимум 3 символа');
-    } else {
-      setValue(requestValue);
-      setIsValueValid(true);
-      setError('');
-    }
-  };
-  const onAddButtonClick = () => {
-    if (value!.length > 2) {
-      setList((prevData) => [
-        ...prevData,
-        { id: prevData.length + 1, value: value },
-      ]);
-      setValue('');
-      setIsValueValid(false);
-    }
-  };
-  console.log(list);
+  // const [value, setValue] = useState<TValue>('');
+  // const [list, setList] = useState<IList[] | []>([]);
+  // const [error, setError] = useState<string>('');
+  // const [isValueVaild, setIsValueValid] = useState<boolean>(false);
+  // const onInputButtonClick = () => {
+  //   const requestValue: string | undefined | null = prompt('Введите значение');
+  //   if (requestValue!.length < 3) {
+  //     setError('Введенное значение должно содержать минимум 3 символа');
+  //   } else {
+  //     setValue(requestValue);
+  //     setIsValueValid(true);
+  //     setError('');
+  //   }
+  // };
+  // const onAddButtonClick = () => {
+  //   if (value!.length > 2) {
+  //     setList((prevData) => [
+  //       ...prevData,
+  //       { id: prevData.length + 1, value: value },
+  //     ]);
+  //     setValue('');
+  //     setIsValueValid(false);
+  //   }
+  // };
+  // console.log(list);
   return (
     <div className={s.appContainer}>
-      <div className={s.app}>
+      {/* <div className={s.app}>
         <h1 className={s['page-heading']}>Ввод значения</h1>
         <p className={s['no-margin-text']}>
           Текущее значение <code>value</code>: "
@@ -66,7 +67,8 @@ function App() {
             )}
           </ul>
         </div>
-      </div>
+      </div> */}
+      <Instruction />
     </div>
   );
 }
---
+++ src/components/Instruction/Instruction.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+import styles from './instraction.module.css';
+import data from '../data.json';
+const Instruction = () => {
+  console.log(data);
+  const [steps] = useState(data);
+  const [activeIndex, setActiveIndex] = useState<number>(1);
+  const [firstStep, setFirstStep] = useState<boolean>(true);
+  const [lastStep, setLastStep] = useState<boolean>(false);
+  const goToNext = () => {
+    if (activeIndex === 7) {
+      setActiveIndex(1);
+    } else {
+      setActiveIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+  useEffect(() => {
+    switch (activeIndex) {
+      case 1:
+        setFirstStep(true);
+        break;
+      case 7:
+        setLastStep(true);
+        break;
+      default:
+        setFirstStep(false), setLastStep(false);
+    }
+  }, [activeIndex]);
+  const goToPrev = () => {
+    setActiveIndex((prevIndex) => prevIndex - 1);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1>Инструкция по готовке пельменей</h1>
+        <div className={styles.steps}>
+          <div className={styles['steps-content']}>
+            {steps[activeIndex - 1]['content']}
+          </div>
+          <ul className={styles['steps-list']}>
+            {steps.map((step, index) => {
+              return (
+                <li
+                  key={step.id}
+                  className={`${styles['steps-item']}  ${
+                    activeIndex === index + 1 ? `${styles.active}` : ''
+                  } ${index < activeIndex && styles.done}`}
+                >
+                  <button
+                    className={styles['steps-item-button']}
+                    onClick={() => setActiveIndex(index + 1)}
+                  >
+                    {index + 1}
+                  </button>
+                  {step.title}
+                </li>
+              );
+            })}
+          </ul>
+          <div className={styles['buttons-container']}>
+            <button
+              className={styles.button}
+              disabled={firstStep}
+              onClick={goToPrev}
+            >
+              Назад
+            </button>
+            <button className={styles.button} onClick={goToNext}>
+              {lastStep ? 'Начать с начала' : 'Далее'}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Instruction;

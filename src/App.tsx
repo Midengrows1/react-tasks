bb984345ee379529e7fc83c185ac31e9149897b1
Midengrows1 <midengrows@outlook.com>
2025-05-06 16:16:07 +0500

    make changes at first task TODO

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,35 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import s from './App.module.css';
 import Instruction from './components/Instruction/Instruction';
-// type TValue = string | null | undefined;
-// interface IList {
-//   id: number;
-//   value: TValue;
-// }
+type TValue = string | null;
+interface IList {
+  id: number;
+  value: TValue;
+}
 function App() {
-  // const [value, setValue] = useState<TValue>('');
-  // const [list, setList] = useState<IList[] | []>([]);
-  // const [error, setError] = useState<string>('');
-  // const [isValueVaild, setIsValueValid] = useState<boolean>(false);
-  // const onInputButtonClick = () => {
-  //   const requestValue: string | undefined | null = prompt('Введите значение');
-  //   if (requestValue!.length < 3) {
-  //     setError('Введенное значение должно содержать минимум 3 символа');
-  //   } else {
-  //     setValue(requestValue);
-  //     setIsValueValid(true);
-  //     setError('');
-  //   }
-  // };
-  // const onAddButtonClick = () => {
-  //   if (value!.length > 2) {
-  //     setList((prevData) => [
-  //       ...prevData,
-  //       { id: prevData.length + 1, value: value },
-  //     ]);
-  //     setValue('');
-  //     setIsValueValid(false);
-  //   }
-  // };
-  // console.log(list);
+  const [value, setValue] = useState<TValue>('');
+  const [list, setList] = useState<IList[] | []>([]);
+  const [error, setError] = useState<string>('');
+  const isValueVaild = !error && value;
+  const onInputButtonClick = () => {
+    const requestValue: string | null = prompt('Введите значение');
+    if (requestValue && requestValue.length < 3) {
+      setError('Введенное значение должно содержать минимум 3 символа');
+    } else {
+      setValue(requestValue);
+      setError('');
+    }
+  };
+  const onAddButtonClick = () => {
+    setList((prevData) => [
+      ...prevData,
+      { id: prevData.length + 1, value: value },
+    ]);
+    setValue('');
+  };
   return (
     <div className={s.appContainer}>
-      {/* <div className={s.app}>
+      <div className={s.app}>
         <h1 className={s['page-heading']}>Ввод значения</h1>
         <p className={s['no-margin-text']}>
           Текущее значение <code>value</code>: "
@@ -67,8 +62,8 @@ function App() {
             )}
           </ul>
         </div>
-      </div> */}
-      <Instruction />
+      </div>
+      {/* <Instruction /> */}
     </div>
   );
 }
